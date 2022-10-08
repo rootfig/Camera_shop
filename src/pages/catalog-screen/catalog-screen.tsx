acdf62845ab7bf27f0ea32a1c349ab7bcdd8f021
Vadim <rootfig@gmail.com>
2022-10-08 12:26:39 +0300

    Добавит header, footer, динамический title

--- src/pages/catalog-screen/catalog-screen.tsx
+++ src/pages/catalog-screen/catalog-screen.tsx
@@ -1,5 +1,19 @@
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import { Helmet } from 'react-helmet';
+
 function CatalogScreen(): JSX.Element {
-  return <p>Каталог товаров</p>;
+  return(
+    <div>
+      <Helmet>
+        <title>Каталог - Фотошоп</title>
+        <meta name="description" content="Фотошоп — Интернет-магазин фото- и видеотехники" />
+      </Helmet>
+      <Header />
+      <p>Каталог товаров</p>;
+      <Footer />
+    </div>
+  );
 }
 
 export default CatalogScreen;
