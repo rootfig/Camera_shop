dd7a991812e709efea61b5a955d1777f16110d9e
Vadim <rootfig@gmail.com>
2022-11-11 19:06:12 +0300

    Исправит форму поиска

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../constants';
+import FormSearch from './form-search/form-search';
 
 
 function Header(): JSX.Element {
@@ -24,28 +25,7 @@ function Header(): JSX.Element {
             </li>
           </ul>
         </nav>
-        <div className="form-search">
-          <form>
-            <label>
-              <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
-                <use xlinkHref="#icon-lens"></use>
-              </svg>
-              <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" />
-            </label>
-            <ul className="form-search__select-list">
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
-            </ul>
-          </form>
-          <button className="form-search__reset" type="reset">
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg><span className="visually-hidden">Сбросить поиск</span>
-          </button>
-        </div>
+        <FormSearch />
         <a className="header__basket-link" href="/#">
           <svg width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-basket"></use>
