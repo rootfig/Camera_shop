f800066e7dc4a08538b8d287961c99a2213732ed
Vadim <rootfig@gmail.com>
2022-12-20 18:48:32 +0300

    Добавит orderPost

--- src/store/basket-slice/selectors.ts
+++ src/store/basket-slice/selectors.ts
@@ -6,4 +6,8 @@ export const selectOrderInGarbage = (state: State) => state[NameSpace.Basket].it
 export const selectIsCameraInBasket = (state: State) => state[NameSpace.Basket].isCameraInBasket;
 export const selectIsAddSuccessItemStatus = (state: State) => state[NameSpace.Basket].isAddSuccessItemStatus;
 export const selectIsRemoveItemStatus = (state: State) => state[NameSpace.Basket].isRemoveItemStatus;
-
+export const selectDiscount = (state: State) => state[NameSpace.Basket].discount;
+export const selectIsCouponLoadError = (state: State) => state[NameSpace.Basket].isCouponLoadError;
+export const selectIsCouponLoaded = (state: State) => state[NameSpace.Basket].isCouponLoaded;
+export const selectOrderPost = (state: State) => state[NameSpace.Basket].order;
+export const selectIsOrderPostStatus = (state: State) => state[NameSpace.Basket].isOrderPostStatus;
