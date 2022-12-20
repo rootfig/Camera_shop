3f0e0968efde03476c09b3be71e26224d01b219e
Vadim <rootfig@gmail.com>
2022-12-20 19:00:28 +0300

    fix

--- src/components/header/header-basket/header-basket.tsx
+++ src/components/header/header-basket/header-basket.tsx
@@ -5,12 +5,13 @@ import { selectOrdersInBasket } from '../../../store/basket-slice/selectors';
 
 function HeaderBasket(): JSX.Element {
   const orders = useAppSelector(selectOrdersInBasket);
+  const ordersCount = orders.length;
   return (
     <Link className="header__basket-link" to={AppRoute.Basket}>
       <svg width="16" height="16" aria-hidden="true">
         <use xlinkHref="#icon-basket"></use>
       </svg>
-      { orders.length > 0 && <span className="header__basket-count">{ orders.length }</span> }
+      { ordersCount > 0 && <span className="header__basket-count">{ ordersCount }</span> }
     </Link>
   );
 }
